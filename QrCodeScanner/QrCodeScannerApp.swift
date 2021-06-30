6d15a89f932a07647fc901d6aa5b624769cb822e
EverlastingHolder <moshkovcevroma@gmail.com>
2021-06-30 22:33:55 +0700

    Initial Commit

---
+++ QrCodeScanner/QrCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QrCodeScannerApp.swift
+//  QrCodeScanner
+//
+//  Created by Роман Мошковцев on 30.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct QrCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
